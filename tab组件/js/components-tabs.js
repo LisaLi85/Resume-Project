a37d8089ea9b970b7c25ed7782ed0765b09e863f
LisaLi85 <756543557@qq.com>
2017-02-11 22:45:22 +0800

    tab组件

--- tab组件/js/components-tabs.js
+++ tab组件/js/components-tabs.js
@@ -13,25 +13,29 @@ Tab.prototype.init = function() {
 Tab.prototype.bind = function(trigType) {
     var _this = this;                           // 先保存this,供以后使用
 
+    function pfunc(para){
+        var target = para.target;              // e.target,当前点的li
+
+        var index = [].indexOf.call(_this.tabLis, target);  
+        // [].indexOf 是借用数组的方法,tabLis本身只是类数组对象。
+        // 在本函数中，this代表被点击的li. 所以用_this.tabLis来表示从 tab-header中找到被点的li 
+
+        _this.tabLis.forEach(function(li) {
+            li.classList.remove('active');          // 去掉标题上li的active.
+        })
+        target.classList.add('active');             // 给被点击的li添加active.     
+
+        _this.tabPanels.forEach(function(panel) {   // 去掉内容上li的active.
+            panel.classList.remove('active');
+        })
+        _this.tabPanels[index].classList.add('active'); // 给被点击的li的内容添加active.          
+    }
+
     if(0==trigType)
     {
         this.tabLis.forEach(function(tabli) {       // 通过 forEach, 将所有的 tab-header>li 都绑定事件。 
             tabli.onclick = function(e) {
-                var target = e.target;              // e.target,当前点的li
-
-                var index = [].indexOf.call(_this.tabLis, target);  
-                // [].indexOf 是借用数组的方法,tabLis本身只是类数组对象。
-                // 在本函数中，this代表被点击的li. 所以用_this.tabLis来表示从 tab-header中找到被点的li 
-
-                _this.tabLis.forEach(function(li) {
-                    li.classList.remove('active');          // 去掉标题上li的active.
-                })
-                target.classList.add('active');             // 给被点击的li添加active.     
-
-                _this.tabPanels.forEach(function(panel) {   // 去掉内容上li的active.
-                    panel.classList.remove('active');
-                })
-                _this.tabPanels[index].classList.add('active'); // 给被点击的li的内容添加active.  
+                pfunc(e);
             }
         })        
     }
@@ -39,21 +43,7 @@ Tab.prototype.bind = function(trigType) {
     {
         this.tabLis.forEach(function(tabli) {       // 通过 forEach, 将所有的 tab-header>li 都绑定事件。 
             tabli.onmouseover = function(e) {
-                var target = e.target;              // e.target,当前点的li
-
-                var index = [].indexOf.call(_this.tabLis, target);  
-                // [].indexOf 是借用数组的方法,tabLis本身只是类数组对象。
-                // 在本函数中，this代表被点击的li. 所以用_this.tabLis来表示从 tab-header中找到被点的li 
-
-                _this.tabLis.forEach(function(li) {
-                    li.classList.remove('active');          // 去掉标题上li的active.
-                })
-                target.classList.add('active');             // 给被点击的li添加active.     
-
-                _this.tabPanels.forEach(function(panel) {   // 去掉内容上li的active.
-                    panel.classList.remove('active');
-                })
-                _this.tabPanels[index].classList.add('active'); // 给被点击的li的内容添加active.  
+                pfunc(e);
             }
         })  
     }
