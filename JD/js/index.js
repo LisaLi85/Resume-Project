23ba5cbfa4b96a1ccdf605356d86811273726bf2
LisaLi85 <756543557@qq.com>
2017-02-19 17:36:33 +0800

    上下导航/backtotop/轮播

--- JD/js/index.js
+++ JD/js/index.js
@@ -1,105 +1,124 @@
-$('.btnclose').on('click',function(){
-    		$('.x22dsk').hide();
-})
-
-
-
-// 大轮播
-      var $view = $('.view')
-      var $img = $('.view').find('li');
-      var imgCount = $img.length;//原始图片个数
-      var imgWidth = $img.width();//单张图片宽度
-      var curIdx = 0;
-      var locked = false;
-      var setTime;
-
-      $view.prepend($img.last().clone());
-      $view.append($img.first().clone());
-      //设置照片容器的初始位置和宽度
-      $view.css({
-        'width':(imgCount+2)*imgWidth,//头尾都另外克隆了一个元素，所以要+2
-        'left':0-imgWidth
+// 头部搜索
+      $(window).on('scroll',function(){
+          if($(window).scrollTop()>50){//当滚动条滚动的距离大于20的时候
+            $('header').css({'background':'#c91523'});//回到顶部按钮显示
+          }else{
+            $('header').css({
+              'background':'none',
+              'opacity':'0.85'
+            });
+          }
       })
-      rollStart();      
-         
-      function rollStart(){
-       // playNext(1)
-       setTime = setInterval(function(){
-              playNext(1)
-       },6000);//每3秒钟执行一次playNext()
-      }
 
-      function playNext(idx){//参数表示每次播放图片的数量
-        var tmpIdx;//定义一个变量，记录下一张图片的索引
-        tmpIdx =(curIdx+idx)%imgCount;   
-        $('.btn').find('li').eq(tmpIdx).addClass('active').siblings().removeClass('active'); 
-        $view.animate({left:'-='+ imgWidth*idx},function(){
-           if(tmpIdx ==0){            
-              $view.css('left',0-imgWidth);
-           }
-            curIdx = tmpIdx; //存储当前的值，供下次使用  
-            locked=false;           
+
+
+// 轮播
+
+var Carousel=(function(){
+  function _Carousel($carousel,_container, _pre, _next, _item_bullet,_autoPlay){
+    this.ct=$carousel;
+      this.init(_container, _pre, _next, _item_bullet);
+      this.bind();
+      if('autoPlay'==_autoPlay)
+      {
+        this.autoPlay();  
+      }
+    
+  }
+
+  _Carousel.prototype.init=function(_container, _pre, _next, _item_bullet){
+    this.$ct = this.ct.find(_container);
+      this.$items=this.$ct.children();
+      this.$pre= this.ct.find(_pre);
+      this.$next=this.ct.find(_next);
+      this.$bullet=this.ct.find(_item_bullet);
+      this.imgWidth=this.$items.width();
+      this.imgCount=this.$ct.children().size();
+
+      this.$ct.prepend(this.$items.last().clone());
+    this.$ct.append(this.$items.first().clone());
+    this.imgRealCount=this.$ct.children().length;
+    this.$ct.css({left:0-this.imgWidth,width:this.imgRealCount*this.imgWidth});
+  };
+  _Carousel.prototype.bind=function(){
+    this.curIdx=0;
+    this.isAnimate=false;
+    var _this=this;
+    this.$next.on('click',function(){
+      _this.playNext();
+    });
+    this.$pre.on('click',function(){
+      _this.playPre();
+    });
+    this.$bullet.find('li').on('click',function(){
+      var idx=$(this).index();
+      if(idx > _this.curIdx){
+        _this.playNext(idx-_this.curIdx);
+      }else if(idx < _this.curIdx){
+        _this.playPre(_this.curIdx-idx);
+      }
+    });
+  };
+  _Carousel.prototype.playNext=function(idx){
+    var idx = idx || 1;
+    var _this=this;
+      if(!this.isAnimate){
+        this.isAnimate=true;
+        this.$ct.animate({left:'-='+(_this.imgWidth*idx)},function(){
+          _this.curIdx+=idx;
+          if(_this.curIdx==_this.imgCount){
+            _this.$ct.css({left:0-_this.imgWidth});
+            _this.curIdx=0;
+          }
+          _this.isAnimate=false;
+          _this.setBullet();
         });
-
       }
-
-      function playPre(idx){
-        var tmpIdx;
-       tmpIdx=(curIdx-idx+imgCount)%imgCount  // +imgCount 是处理curIdx为负1的情况.
-       $('.btn').find('li').eq(tmpIdx).addClass('active').siblings().removeClass('active');
-        $view.animate({left:'+='+ (imgWidth*idx)},function(){           
-           if(tmpIdx ==imgCount-1){
-              $view.css('left',0-imgWidth*imgCount);
-           } 
-            curIdx = tmpIdx; //存储当前的值，供下次使用 
-            locked=false;           
-        });                        
+  };
+  _Carousel.prototype.playPre=function(idx){
+    var idx=idx||1;
+    var _this=this;
+      if(!this.isAnimate){
+        this.isAnimate=true;
+        this.$ct.animate({left:'+='+(_this.imgWidth*idx)},function(){
+          _this.curIdx=(_this.imgCount+_this.curIdx-idx)%_this.imgCount;
+          if(_this.curIdx==(_this.imgCount-1)){
+            _this.$ct.css({left:0-_this.imgWidth*_this.imgCount});
+          }
+          _this.isAnimate=false;
+          _this.setBullet();
+        });
       }
+  };
+  _Carousel.prototype.setBullet=function(){
+    this.$bullet.find('li').removeClass('active')
+                        .eq(this.curIdx).addClass('active');
+  };
+  _Carousel.prototype.autoPlay=function(){
+    var _this=this;
+    this.clock=setInterval(function(){
+        _this.playNext();
+    },3000);
+  };
+  _Carousel.prototype.stopAuto=function(){
+    clearInterval(this.clock);
+  };
 
-      $('.right').on('click',function(){
-        if(locked){
-              return;
-        }
-        locked = true;
-        clearInterval(setTime);
-        playNext(1);
+  return {
+    // init:function($ct,_container, _pre, _next, _item_bullet,_autoPlay){
+    //  $ct.each(function(index,node){
+    //    new _Carousel($(node),_container, _pre, _next, _item_bullet,_autoPlay);
+    //  });
+    // }
 
-        rollStart();
-      });
+    init:function($ct,_container, _pre, _next, _item_bullet,_autoPlay){
+      
+      new _Carousel($ct,_container, _pre, _next, _item_bullet,_autoPlay);
+      
+    }
 
-      $('.left').on('click',function(){
-        if(locked){
-              return;
-        }
-        locked = true;
-        clearInterval(setTime);
-        playPre(1);
-
-        rollStart();
-      });
-
-      $('.btn').find('li').on('click',function(){
-       var index = $(this).index();//获取当前点击的图片
-       if(locked){
-              return;
-       }
-
-       clearInterval(setTime);            
-       locked = true;
-
-
-
-       if(index>curIdx){
-              playNext(index-curIdx);
-       }
-       if(index<curIdx){
-              playPre(curIdx-index);
-       }
-
-
-       rollStart();
-      })
-
+  };
+})();
 
 
 // 懒加载
@@ -136,3 +155,16 @@ $('.btnclose').on('click',function(){
 					}
 				})
 			}
+
+
+//回到顶部
+      $(window).on('scroll',function(){
+          if($(window).scrollTop()>80){//当滚动条滚动的距离大于80的时候
+            $('.BackToTop').show();//回到顶部按钮显示
+          }else{
+            $('.BackToTop').hide();
+          }
+      })
+      $('.BackToTop').on('click',function(){//点击回到顶部按钮，跳转到页面顶部
+          $(window).scrollTop(0);
+      })
